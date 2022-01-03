6e9617140cedd150dce04372374b3c8cc89b7e7b
avedyer <avedyer@gmail.com>
2022-01-03 17:14:04 -0500

    early object frameworks

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
 const Ship = (length) => {
-    let hits = []
+
+    let hits = [];
+    let vertical = true;
+    let coords = []
 
     function hit(position) {
-        if (hits > length || hits < 0) {
+        if (position > length || position < 1 || hits.includes(position)) {
             return false
         }
         hits.push(position);
@@ -11,11 +14,69 @@ const Ship = (length) => {
         return true
     }
 
+    function isSunk() {
+        for (let i=1; i<=length; ++i) {
+            if (!hits.includes(i)) {
+                return false
+            }
+            return true
+        }
+    }
+
     return {
         length,
         hits,
-        hit
+        coords,
+        vertical,
+        hit,
+        isSunk
     }
 }
 
-export {Ship}
+
+const Board = () => {
+
+    let attacks = [];
+    let hits = [];
+    let ships = [];
+    let shipCoords = [];
+
+    function placeShip(ship, coord) {
+        ships.push(ship);
+        for (let i=0; i<ship.length; ++i) {
+            ship.coords.push(
+                ship.vertical ? [coord[0], coord[1] + i] : [coord[0] + i, coord[1]]
+            );
+        }
+
+    }
+
+    function receiveAttack(attack) {
+        if (attacks.includes(attack)) {
+            return false
+        }
+
+        attacks.push(attack);
+        
+        for (let ship of ships) {
+            for (let coord of ship.coords) {
+                if (attack[0] === coord[0] && attack[1] === coord[1]) {
+                    hits.push(attack);
+                    ship.hit
+                }
+            }
+        }
+
+        return true
+
+    }
+    return {
+        ships,
+        attacks,
+        hits,
+        placeShip,
+        receiveAttack
+    }
+}
+
+export {Ship, Board}
--- game.test.js
+++ game.test.js
@@ -1,10 +1,78 @@
-import {Ship} from './game.js'
+import {Ship, Board} from './game.js'
 
-it('Register Hit', () => {
+it('Registers Hit', () => {
 
     let testShip = Ship(4);
     testShip.hit(2)
 
     expect(testShip.hits.includes(2))
     .toBe(true);
+})
+
+it('Deny invalid hit location', () => {
+    let testShip = Ship(3);
+    expect(testShip.hit(5))
+    .toBe(false);
+})
+
+it('Sinks ship', () => {
+    let testShip = Ship(4);
+
+    for(let i=1; i<5; ++i) {
+        testShip.hit(i);
+    }
+
+    expect(testShip.isSunk())
+    .toBe(true);
+})
+
+it('Places Ship', () => {
+
+    let testBoard = Board();
+    let testShip = Ship(3)
+
+    testBoard.placeShip(testShip, [4,3]);
+
+    expect(testBoard.ships)
+    .toStrictEqual([
+        testShip
+    ]);
+});
+
+it('Registers ship coordinates', () => {
+    
+    let testBoard = Board();
+    let testShip = Ship(3);
+
+    testBoard.placeShip(testShip, [3,6]);
+
+    expect(testShip.coords)
+    .toStrictEqual([
+        [3,6],
+        [3,7],
+        [3,8]
+    ]);
+});
+
+it('Receives Attack', () => {
+
+    let testBoard = Board();
+
+    testBoard.receiveAttack([4,6]);
+
+    expect(testBoard.attacks)
+    .toStrictEqual([[4,6]]);
+});
+
+it('Registers Hit', () => {
+
+    let testBoard = Board();
+    let testShip = Ship(3);
+
+    testBoard.placeShip(testShip, [3,6]);
+    testBoard.receiveAttack([3,8]);
+
+    expect(testBoard.hits)
+    .toStrictEqual([[3,8]]);
+
 })
